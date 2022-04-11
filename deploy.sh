c9ff2ebfa133eee79e4292d2614dc1071d75c9b9
Steve R. Smith <83226400+contraster-steve@users.noreply.github.com>
2022-04-11 16:28:40 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ sudo rm -rf /home/ubuntu/WebGoat.tar.gz
 echo "Start containers"
 cd /home/ubuntu/webapps/WebGoat/
 sudo chown -R ubuntu:ubuntu /home/ubuntu/webapps/WebGoat
-/usr/bin/java -Dfile.encoding=UTF-8 -Dserver.port=5005 -Dserver.address=localhost -Dhsqldb.port=9001 -javaagent:./WebGoat/webgoat-standalone/target/contrast.jar -Dcontrast.config.path=./WebGoat/webgoat-standalone/target/contrast_security.yaml -jar ./WebGoat/webgoat-container/target/webgoat-container-7.1-war-exec.jar </dev/null &>/dev/null &
+/usr/bin/java -Dfile.encoding=UTF-8 -Dserver.port=5005 -Dserver.address=localhost -Dhsqldb.port=9001 -javaagent:./webgoat-standalone/target/contrast.jar -Dcontrast.config.path=./webgoat-standalone/target/contrast_security.yaml -jar ./webgoat-container/target/webgoat-container-7.1-war-exec.jar </dev/null &>/dev/null &
 sleep 25
 echo "Curl URLS"
 sudo curl --silent --output /dev/null -X GET "https://webgoat.contrast.pw/"
